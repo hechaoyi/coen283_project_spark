75b7320b460b80fb69022eafd4852474f4d80515
hechaoyi <hechaoyi@me.com>
2018-06-01 14:41:05 -0700

    tab -> 4 spaces

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 ## Data Source: [Ford GoBike - System Data](https://www.fordgobike.com/system-data)
+
+
+```bash
+cd idle_time
+mvn package
+spark-submit --executor-memory 4G --class RunIdleTime target/spark-idle-time-1.0-SNAPSHOT-jar-with-dependencies.jar
+```
--- idle_time/src/main/scala/RunIdleTime.scala
+++ idle_time/src/main/scala/RunIdleTime.scala
@@ -14,23 +14,23 @@ object RunIdleTime {
 
     val stationPopular = popular.groupBy("station").count  // 298
     val stationPeriodPopular = popular.groupBy("station", "period").count.
-	  groupByKey(row => row.getInt(0)).mapGroups {
-	    case (station, rows) => (station, rows.map(row => Seq(row.getInt(1), row.getLong(2).toInt)).toSeq.sortBy(_(0)))
-	  }.toDF("station", "periods")
-	val stationPopularJoined = stationPopular.join(stationPeriodPopular,
-	  stationPopular.col("station") === stationPeriodPopular.col("station"))
+      groupByKey(row => row.getInt(0)).mapGroups {
+        case (station, rows) => (station, rows.map(row => Seq(row.getInt(1), row.getLong(2).toInt)).toSeq.sortBy(_(0)))
+      }.toDF("station", "periods")
+    val stationPopularJoined = stationPopular.join(stationPeriodPopular,
+      stationPopular.col("station") === stationPeriodPopular.col("station"))
 
-	val sessions = clean.repartition($"bikeId").sortWithinPartitions($"bikeId", $"startTime")
-	val idle = sessions.mapPartitions(trips => {
-	  trips.sliding(2)
-	    .filter { case a::b::Nil =>
-	      a.bikeId == b.bikeId && a.endStationId == b.startStationId }
-	    .map { case a::b::Nil => (a.endStationId, a.endHour, ((b.startTime-a.endTime)/1000).toInt) }
-	}).toDF("station", "period", "seconds").cache
-	idle.createOrReplaceTempView("idle")
+    val sessions = clean.repartition($"bikeId").sortWithinPartitions($"bikeId", $"startTime")
+    val idle = sessions.mapPartitions(trips => {
+      trips.sliding(2)
+        .filter { case a::b::Nil =>
+          a.bikeId == b.bikeId && a.endStationId == b.startStationId }
+        .map { case a::b::Nil => (a.endStationId, a.endHour, ((b.startTime-a.endTime)/1000).toInt) }
+    }).toDF("station", "period", "seconds").cache
+    idle.createOrReplaceTempView("idle")
 
 /*
-	spark sql """
+    spark sql """
 select station,
   percentile_approx(seconds,0.01) p1,
   percentile_approx(seconds,0.02) p2,
@@ -51,42 +51,42 @@ from idle group by period
 """
 */
 
-	val stationIdle = spark.sql(
-	  "select station, percentile_approx(seconds,0.03) p3 from idle group by station")  // 298
-	val stationPeriodIdle = spark.sql(
-	  "select station, period, percentile_approx(seconds,0.03) p3 from idle group by station, period").
-	  groupByKey(row => row.getInt(0)).mapGroups {
-	    case (station, rows) => (station, rows.map(row => Seq(row.getInt(1), row.getInt(2))).toSeq.sortBy(_(0)))
-	  }.toDF("station", "periods")
-	val stationIdleJoined = stationIdle.join(stationPeriodIdle,
-	  stationIdle.col("station") === stationPeriodIdle.col("station"))
+    val stationIdle = spark.sql(
+      "select station, percentile_approx(seconds,0.03) p3 from idle group by station")  // 298
+    val stationPeriodIdle = spark.sql(
+      "select station, period, percentile_approx(seconds,0.03) p3 from idle group by station, period").
+      groupByKey(row => row.getInt(0)).mapGroups {
+        case (station, rows) => (station, rows.map(row => Seq(row.getInt(1), row.getInt(2))).toSeq.sortBy(_(0)))
+      }.toDF("station", "periods")
+    val stationIdleJoined = stationIdle.join(stationPeriodIdle,
+      stationIdle.col("station") === stationPeriodIdle.col("station"))
 
-	val stationInfo = data.select(
-	  $"start_station_id" as "id",
-	  $"start_station_name" as "name",
-	  $"start_station_latitude" as "lat",
-	  $"start_station_longitude" as "long").distinct.union(
-	  data.select("end_station_id", "end_station_name",
-	    "end_station_latitude", "end_station_longitude").distinct
-	).distinct.filter("name not like '%Coming Soon'").cache  // 298
+    val stationInfo = data.select(
+      $"start_station_id" as "id",
+      $"start_station_name" as "name",
+      $"start_station_latitude" as "lat",
+      $"start_station_longitude" as "long").distinct.union(
+      data.select("end_station_id", "end_station_name",
+        "end_station_latitude", "end_station_longitude").distinct
+    ).distinct.filter("name not like '%Coming Soon'").cache  // 298
 
-	val result1 = stationIdleJoined.join(stationInfo, stationIdle.col("station") === stationInfo.col("id"))
-	val json1 = result1.map(row =>
-	  (row.getAs[String]("name"), Seq(
-	    row.getAs[String]("long").toDouble,
-	    row.getAs[String]("lat").toDouble,
-	    row.getAs[Int]("p3")), row.getAs[Seq[Seq[Int]]]("periods"))
-	).toDF("name", "value", "periods")
-	json1.coalesce(1).write.json("out1")
+    val result1 = stationIdleJoined.join(stationInfo, stationIdle.col("station") === stationInfo.col("id"))
+    val json1 = result1.map(row =>
+      (row.getAs[String]("name"), Seq(
+        row.getAs[String]("long").toDouble,
+        row.getAs[String]("lat").toDouble,
+        row.getAs[Int]("p3")), row.getAs[Seq[Seq[Int]]]("periods"))
+    ).toDF("name", "value", "periods")
+    json1.coalesce(1).write.json("out1")
 
-	val result2 = stationPopularJoined.join(stationInfo, stationPopular.col("station") === stationInfo.col("id"))
-	val json2 = result2.map(row =>
-	  (row.getAs[String]("name"), Seq(
-	    row.getAs[String]("long").toDouble,
-	    row.getAs[String]("lat").toDouble,
-	    row.getAs[Long]("count")), row.getAs[Seq[Seq[Int]]]("periods"))
-	).toDF("name", "value", "periods")
-	json2.coalesce(1).write.json("out2")
+    val result2 = stationPopularJoined.join(stationInfo, stationPopular.col("station") === stationInfo.col("id"))
+    val json2 = result2.map(row =>
+      (row.getAs[String]("name"), Seq(
+        row.getAs[String]("long").toDouble,
+        row.getAs[String]("lat").toDouble,
+        row.getAs[Long]("count")), row.getAs[Seq[Seq[Int]]]("periods"))
+    ).toDF("name", "value", "periods")
+    json2.coalesce(1).write.json("out2")
 
     spark.stop
   }
